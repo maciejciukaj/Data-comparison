10225a08f8ef9fede4d4b7070a2d6214caa984f6
maciej_ciukaj <maciek.ciukaj@gmail.com>
2022-05-10 19:03:32 +0200

    costam

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,7 +26,7 @@ namespace API
 
         
 
-        // This method gets called by the runtime. Use this method to add servicess to the container.
+        // This method gets called by the runtime. Use this method to add servicess to thes container.
         public void ConfigureServices(IServiceCollection services)
         {
 
